dcee24abe03b0971d35ec6accb1eb5b9b1e9c270
Lyssa_ramoss <melyssaramos98@gmail.com>
2023-02-28 15:07:48 +0000

    conta1

---
+++ Programa.java
@@ -0,0 +1,21 @@
+class Programa{
+    public static void main(String[] args){
+    Conta minhaConta;
+    minhaConta=new Conta();
+    minhaConta.numero=1;
+    minhaConta,titular="Melzinha Ramos";
+    minhaConta.saldo=673;
+
+    Conta minhaConta2;
+    minhaConta2=new Conta();
+    minhaConta2.numero=2;
+    minhaConta2.titular="Ester";
+    minhaConta2.saldo=1000;
+
+    System.out.println(minhaConta.numero);
+    System.out.println(minhaConta.titular);
+    System.out.println(minhaConta.saldo);
+
+   System.out.println(x:"/n");
+}
+}
